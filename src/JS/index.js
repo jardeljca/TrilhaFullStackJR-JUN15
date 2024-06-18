93032ab0ed84d572b37ae3405593fc1fdcaf81dc
Jardel Queiroz <jardeljcadm@gmail.com>
2024-06-18 02:40:55 -0300

    Conclusão do main ad página principal

--- src/JS/index.js
+++ src/JS/index.js
@@ -2,7 +2,7 @@ const botaoMenu = document.getElementById("iconeMenu");
 const botaoMenuX = document.getElementById("iconeMenuX");
 const menuNav = document.getElementById("menuNav");
 const header = document.getElementById("header");
-//const main = document.getElementById("main");
+const main = document.getElementById("main");
 var larguraDaJanela = window.innerWidth;
 
 menuNav.style.display = "none";
@@ -11,13 +11,13 @@ if (larguraDaJanela < 560) {
   
     botaoMenu.addEventListener("click", () => {
       header.style.display = "none";
-      //main.style.display = "none";
+      main.style.display = "none";
       menuNav.style.display = "flex";
     });
 
     botaoMenuX.addEventListener("click", () => {
       menuNav.style.display = "none";
       header.style.display = "flex";
-      //main.style.display = "flex";
+      main.style.display = "flex";
     });
 }
